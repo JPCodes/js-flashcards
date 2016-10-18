16b7ad212d020ae2714cf5b55440b49d89a5634c
Erik Killops and Jim Padilla <jim@>
2016-10-18 11:15:10 -0700

    Add list items, hide and show jquery functionality, defined terms

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+  $("#javascript").click(function() {
+    $("#javascript-def").toggle();
+  });
+
+  $("#operators").click(function() {
+    $("#operators-def").toggle();
+  });
+
+  $("#conventions").click(function() {
+    $("#conventions-def").toggle();
+  });
+
+  $("#functions").click(function() {
+    $("#functions-def").toggle();
+  });
+
+  $("#methods").click(function() {
+    $("#methods-def").toggle();
+  });
+
+  $("#booleans").click(function() {
+    $("#booleans-def").toggle();
+  });
+
+  $("#undefined").click(function() {
+    $("#undefined-def").toggle();
+  });
+
+  $("#nan").click(function() {
+    $("#nan-def").toggle();
+  });
+
+  $("#escape").click(function() {
+    $("#escape-def").toggle();
+  });
+  $("#alert").click(function() {
+    $("#alert-def").toggle();
+  });
+
+});
